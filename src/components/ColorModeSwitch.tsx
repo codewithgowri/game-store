6e953a7a61c3c665e88fcf2fbb305122117bbb0b
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-19 15:04:52 +0530

    base screen bug fix,Build ColorModeToggle Switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack padding="10px">
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      ></Switch>
+      <Text>Dark mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;

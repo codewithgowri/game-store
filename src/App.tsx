b8a8cc617c101c80b15d5a3c7bd1dbf0b696291e
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-19 13:46:27 +0530

    Build a Response Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={"brown"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg={"red"}>
+        main
+      </GridItem>
+    </Grid>
   );
 }
 

d7eced4457cc273ed3aead3ad402239ce6452c88
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-20 13:04:55 +0530

    Build SKeleton GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   const { parent_platforms } = game;
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card width={"250px"} borderRadius={10} overflow={"hidden"}>
       <Image src={optimizeImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl"> {game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/GetGamesHook";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       {error && <p>error</p>}
@@ -12,6 +14,8 @@ const GameGrid = () => {
         spacing={10}
         padding={10}
       >
+        {isLoading &&
+          skeletons.map((e) => <GameCardSkeleton></GameCardSkeleton>)}
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}

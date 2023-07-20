5d07296fe980b5c8a0814116d30880f421a545e2
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-20 11:42:05 +0530

    getOptimized Image URL

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Game } from "../hooks/GetGamesHook";
 import PlatformIcons from "./PlatformIcons";
 import ScoreBadge from "./ScoreBadge";
+import optimizeImageUrl from "../services/optimize-image-url";
 interface Props {
   game: Game;
 }
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
   const { parent_platforms } = game;
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image}></Image>
+      <Image src={optimizeImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl"> {game.name}</Heading>
 

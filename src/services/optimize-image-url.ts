bc830abb4a54a70e33251d359afd22526f39b21f
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-21 15:08:19 +0530

    Handle Games with no image

--- src/services/optimize-image-url.ts
+++ src/services/optimize-image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../../src/assets/no-image-placeholder.webp";
 const optimizeImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
